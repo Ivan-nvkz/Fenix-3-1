bd2e00dfb5704fe70d10ff52de199a3833a999df
Ivan-nvkz <66069539+Ivan-nvkz@users.noreply.github.com>
2022-06-20 07:43:05 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -448,7 +448,23 @@ $(document).ready(function () {
 // Slick slider start ====================================================================
 $(function () {
    $('.header-slider').slick({
+      adaptiveHeight:false,
       dots:true,
+      speed:1000,
+      autoplay:true,
+      autoplaySpeed:3000,
+       responsive: [
+        {
+          breakpoint: 650,
+          settings: {
+            speed:300,
+            adaptiveHeight:true,
+            autoplay:false,
+          }
+        },
+       
+      ],
+
       prevArrow: '<button class="header-slider-btn header-slider-btn__left"><svg width="85" height="85" viewBox="0 0 85 85" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="85" height="85" transform="matrix(-1 0 0 1 85 0)" fill="white"/><path fill-rule="evenodd" clip-rule="evenodd" d="M38.7761 43.1667H48.5V41.8334H38.7761L42.9714 37.6381L42.0286 36.6953L36.2239 42.5001L42.0286 48.3048L42.9714 47.362L38.7761 43.1667Z" fill="#BF9D77"/></svg></button>',
       nextArrow: '<button class="header-slider-btn header-slider-btn__right"><svg width="136" height="85" viewBox="0 0 136 85" fill="none" xmlns="http://www.w3.org/2000/svg">   <rect x="0.5" y="0.5" width="136" height="85" fill="#BF9D77"/>    <rect x="0.5" y="0.5" width="136" height="85" stroke="#BF9D77"/>         <path  fill-rule="evenodd" clip-rule="evenodd" d="M103.724 43.4714H30V42.1381H103.724L99.5286 37.9428L100.471 37L106.276 42.8047L100.471 48.6095L99.5286 47.6667L103.724 43.4714Z" fill="white"/>         </svg></button>',
      
